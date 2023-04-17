7c768e066c56851000f2366ab76f11f6646d462d
MiloLurati <lurati.milo@gmail.com>
2023-04-17 14:18:07 +0200

    update

--- test/test_hip_functions.py
+++ test/test_hip_functions.py
@@ -28,10 +28,10 @@ def test_ready_argument_list():
 
     assert(gpu_args, ctypes.Structure)
     
-    assert isinstance(gpu_args[0], ctypes.c_void_p)
-    assert isinstance(gpu_args[1], ctypes.c_int32)
-    assert isinstance(gpu_args[2], ctypes.c_void_p)
-    assert isinstance(gpu_args[3], ctypes.c_bool)
+    assert isinstance(gpu_args.field0, ctypes.c_void_p)
+    assert isinstance(gpu_args.field1, ctypes.c_int32)
+    assert isinstance(gpu_args.field2, ctypes.c_void_p)
+    assert isinstance(gpu_args.field3, ctypes.c_bool)
 
 @skip_if_no_pyhip
 def test_compile():
