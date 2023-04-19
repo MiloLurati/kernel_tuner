405a243f1435c62f527d851ca8017c6c2f62ec6a
MiloLurati <lurati.milo@gmail.com>
2023-04-19 15:54:02 +0200

    update

--- test/test_hip_functions.py
+++ test/test_hip_functions.py
@@ -117,8 +117,8 @@ def test_copy_constant_memory_args():
     gpu_args = dev.ready_argument_list([output])
     threads = (100, 1, 1)
     grid = (1, 1, 1)
-    dev.run_kernel(kernel, gpu_args, threads, grid)
     print(f'gpu_args[0] = {gpu_args[0]}')
+    dev.run_kernel(kernel, gpu_args, threads, grid)
     dev.memcpy_dtoh(output, gpu_args[0])
     for i, arg in enumerate(output):
         print(f'output[{i}] = {arg}')
